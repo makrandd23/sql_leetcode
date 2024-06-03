95d088f2ece588a27f8219fa0e579e55ff4c2d19
makrandd23 <makranddeshmukh23@gmail.com>
2024-06-04 01:07:29 +0530

    Update department top three salaries.sql

--- department top three salaries.sql
+++ department top three salaries.sql
@@ -1,16 +1,14 @@
-SELECT 
-  B.NAME AS DEPARTMENT,
-  A.NAME AS EMPLOYEE,
-  A.SALARY
-FROM 
-  (
-  SELECT 
-    NAME,
-    SALARY,
-    DEPARTMENTID,
-    DENSE_RANK() OVER (PARTITION BY DEPARTMENTID ORDER BY SALARY DESC) AS RNK
-  FROM EMPLOYEE
-) A LEFT OUTER JOIN 
-DEPARTMENT B
-ON B.ID = A.DEPARTMENTID
-WHERE A.RNK <= 3
+SELECT B.NAME AS DEPARTMENT,
+       A.NAME AS EMPLOYEE,
+       A.salary
+FROM   (SELECT NAME,
+               salary,
+               departmentid,
+               Dense_rank()
+                 OVER (
+                   partition BY departmentid
+                   ORDER BY salary DESC) AS RNK
+        FROM   employee) A
+       LEFT OUTER JOIN department B
+                    ON B.id = A.departmentid
+WHERE  A.rnk <= 3 
